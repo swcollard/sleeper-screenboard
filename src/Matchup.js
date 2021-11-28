d67204a835fd14755f66e6dbe0e77e4ea22aae02
swcollard <swcollard12@gmail.com>
2021-11-28 12:27:26 -0600

    Bold player in the lead

--- src/Matchup.js
+++ src/Matchup.js
@@ -17,23 +17,39 @@ class Matchup extends React.Component {
         const rosterId1 = player1["roster_id"];
         const userId1 = rosters.find(roster => roster["roster_id"] === rosterId1)["owner_id"];
         const teamName1 = users.find(user => user["user_id"] === userId1)["metadata"]["team_name"];
+        const player1Points = player1["points"];
         const rosterId2 = player2["roster_id"];
         const userId2 = rosters.find(roster => roster["roster_id"] === rosterId2)["owner_id"];
         const teamName2 = users.find(user => user["user_id"] === userId2)["metadata"]["team_name"];
+        const player2Points = player2["points"];
+        if (player1Points == player2Points) {
+            return (
+                <div className="match-row">
+                    <p>Team: {teamName1}</p>
+                    <p>Points: {player1Points}</p>
+                    <p>Team: {teamName2}</p>
+                    <p>Points: {player2Points}</p>
+                    <br />
+                </div>
+            );
+        }
+        if (player1Points > player2Points) {
+            return (
+                <div className="match-row">
+                    <b><p>Team: {teamName1}</p>
+                    <p>Points: {player1Points}</p></b>
+                    <p>Team: {teamName2}</p>
+                    <p>Points: {player2Points}</p>
+                    <br />
+                </div>
+            );
+        }
         return (
             <div className="match-row">
-                <p>
-                    Team: {teamName1}
-                </p>
-                <p>
-                    Points: {player1["points"]}
-                </p>
-                <p>
-                    Team: {teamName2}
-                </p>
-                <p>
-                    Points: {player2["points"]}
-                </p>
+                <p>Team: {teamName1}</p>
+                <p>Points: {player1Points}</p>
+                <b><p>Team: {teamName2}</p>
+                <p>Points: {player2Points}</p></b>
                 <br />
             </div>
         );
